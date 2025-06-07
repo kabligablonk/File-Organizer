7aaba5b198b5cbc60bb691847e51615f0b5b6ee3
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-06-07 16:12:38 +0800

    Added input and error handling

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import shutil
 from os import listdir
 
-source_folder = "C:\\Users\\aeggn\\OneDrive\\Desktop\\Test"
-destination_folder = ""
+source_folder = input("Folder to be sorted:")
 
 current_file_types = []
 
@@ -11,6 +10,9 @@ for file in listdir(source_folder):
     file_name, file_type= os.path.splitext(file)
     current_file_types.append(file_type)
 for folder_file_type in current_file_types:
-    full_path = os.path.join(source_folder, folder_file_type)
-    os.mkdir(full_path)
-    print(full_path)
+    try:
+        full_path = os.path.join(source_folder, folder_file_type)
+        os.mkdir(full_path)
+        print(f"Created Folder: '{folder_file_type}': Folder location: {full_path}")
+    except FileExistsError as e:
+        continue
