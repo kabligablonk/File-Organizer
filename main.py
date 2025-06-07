34d7541b00fdadbdc9091c2599f41c3504533e39
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-06-08 04:44:17 +0800

    Moves files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import shutil
 from os import listdir
 
@@ -9,10 +10,13 @@ current_file_types = []
 for file in listdir(source_folder):
     file_name, file_type= os.path.splitext(file)
     current_file_types.append(file_type)
-for folder_file_type in current_file_types:
-    try:
-        full_path = os.path.join(source_folder, folder_file_type)
-        os.mkdir(full_path)
-        print(f"Created Folder: '{folder_file_type}': Folder location: {full_path}")
-    except FileExistsError as e:
-        continue
+    for folder_file_type in current_file_types:
+        try:
+            full_file_path = os.path.join(source_folder, folder_file_type)
+            os.mkdir(full_file_path)
+            print(f"Created Folder: '{folder_file_type}': Folder location: {full_file_path}")
+            moving_file = os.path.join(source_folder, file)
+            shutil.move(moving_file, full_file_path)
+            print(f"Moved '{file}' to '{full_file_path}'")
+        except FileExistsError as e:
+            continue
