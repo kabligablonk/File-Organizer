56057a32742a752690b411fa49b96e250ab73f59
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-06-10 18:39:50 +0800

    Added error handling and print messages

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from os import listdir
 
 source_folder = input("Folder to be sorted:")
 
-
-for file in listdir(source_folder):
-    try:
-        file_name, file_type= os.path.splitext(file)
-        full_folder_path = os.path.join(source_folder, file_type)
-        moving_file = os.path.join(source_folder, file)
-        if not os.path.exists(full_folder_path):
-            os.mkdir(full_folder_path)
-        shutil.move(moving_file, full_folder_path)
-    except shutil.Error:
-        continue
+try:
+    for file in listdir(source_folder):
+        try:
+            file_name, file_type= os.path.splitext(file)
+            full_folder_path = os.path.join(source_folder, file_type)
+            moving_file = os.path.join(source_folder, file)
+            if not os.path.exists(full_folder_path):
+                os.mkdir(full_folder_path)
+                print(f"Created folder: {full_folder_path}")
+            shutil.move(moving_file, full_folder_path)
+            print(f"Moved: '{file}' to {full_folder_path}")
+        except shutil.Error:
+            continue
+except FileNotFoundError:
+    quit()
