76cf2ad09a84e9fc2b14d6b61524a8f46f89fc1c
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-06-10 16:30:45 +0800

    Added error handling

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from os import listdir
 
 source_folder = input("Folder to be sorted:")
 
+
 for file in listdir(source_folder):
-    file_name, file_type= os.path.splitext(file)
-    full_folder_path = os.path.join(source_folder, file_type)
-    moving_file = os.path.join(source_folder, file)
-    if not os.path.exists(full_folder_path):
-        os.mkdir(full_folder_path)
-    shutil.move(moving_file, full_folder_path)
+    try:
+        file_name, file_type= os.path.splitext(file)
+        full_folder_path = os.path.join(source_folder, file_type)
+        moving_file = os.path.join(source_folder, file)
+        if not os.path.exists(full_folder_path):
+            os.mkdir(full_folder_path)
+        shutil.move(moving_file, full_folder_path)
+    except shutil.Error:
+        continue
